72a71e87eb1bf1301b5f9b8a68e904740b83e55b
Rfgfgfgfgf <rfgfgfgfgf3456787@gmail.com>
2024-10-19 10:16:04 +0400

    pp

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def go(message):
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
-        bot.send_message(message.chat.id, pokemon.get_food())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
